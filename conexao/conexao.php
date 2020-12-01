7b4a509e3f033cdb2adcc4e21803e430432398ff
francisoeste <francisoeste2@gmail.com>
2020-11-30 23:44:53 -0300

    configuração conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,12 +1,38 @@
-<?php 
-    $ip      = "localhost";
-    $usuario = "root";
-    $senha   = "";
-    $banco   = "atendimento";
-    
-    $conecta = mysqli_connect($ip, $usuario, $senha, $banco);
+<?php
 
-    if ( mysqli_connect_error() ) {
-        die("Erro ao Conectar no Servidor" . mysqli_connect_error());
+class Sql extends PDO {
+
+    private $conn;
+
+    public function __construct() {
+        
+        $this->conn = new PDO("mysql:dbanem=atendimento;host=localhost" , "root", "");
+
     }
+
+    private function setParams($statment, $parameters = array()) {
+
+        foreach ($parameters as $key => $value) {
+
+            $this->setParam($key, $value);
+
+        }
+
+    }
+
+    private function setParam($statment, $key, $value) {
+
+        $statment->bindParam($key, $value);
+    }
+
+    public function query($rowQuery, $params = array()){
+
+        $stmt = $this->conn->prepare($rowQuery);
+
+        $this->setParams($stmt, $params);
+
+        return $stmt->execute();
+    }
+}
+
 ?>
